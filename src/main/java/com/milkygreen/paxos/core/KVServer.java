8c223dde296e2edd4d0ca3daeae945146eab1faa
李云梦 <yunmeng.li@successchannel.com>
2022-02-21 17:18:33 +0800

    完善核心方法

--- src/main/java/com/milkygreen/paxos/core/KVServer.java
+++ src/main/java/com/milkygreen/paxos/core/KVServer.java
@@ -38,16 +38,23 @@ public class KVServer {
     }
 
 
+    /**
+     * 处理写前读方法
+     * @param proposer
+     * @return
+     */
     public Acceptor Prepare(Proposer proposer){
         Version lockedVersion = null;
         try {
+            // 获取要处理的key版本
             lockedVersion = getLockedVersion(proposer.getId());
 
             Acceptor acceptor = lockedVersion.getAcceptor();
-            if(acceptor == null){
+            if(acceptor == null){ // 还没被写入过，将LastBal设置为proposer的
                 acceptor = new Acceptor();
                 acceptor.setLastBal(proposer.getBal());
             }else{
+                // 写入过且记录的LastBal小于proposer的，记录proposer的bal。否则什么也不做。
                 if(proposer.getBal().greaterThen(acceptor.getLastBal())){
                     acceptor.setLastBal(proposer.getBal());
                 }
@@ -58,12 +65,19 @@ public class KVServer {
         }
     }
 
+    /**
+     * 写入方法
+     * @param proposer
+     * @return
+     */
     public Acceptor accept(Proposer proposer){
         Version lockedVersion = null;
         try {
             lockedVersion = getLockedVersion(proposer.getId());
             Acceptor acceptor = lockedVersion.getAcceptor();
+            // 只接收bal大于等于lockedVersion里面lastBal的写入请求，其他的一概不管
             if(proposer.getBal().greaterThen(acceptor.getLastBal())){
+                // 更新这个key版本的相应值
                 acceptor.setVal(proposer.getVal());
                 acceptor.setLastBal(proposer.getBal());
                 acceptor.setValBal(proposer.getBal());
